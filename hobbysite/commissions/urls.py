95686103df9b904ccbf40cc5c54c5d737e2d36dd
charmnaciongayo-shopee <maria.naciongayo@shopee.com>
2024-03-19 21:31:02 +0800

    Initial commit for commissions app

---
+++ hobbysite/commissions/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'commissions'
+
+urlpatterns = [
+    path('list/', views.CommissionListView.as_view(), name='commission_list'),
+    path('detail/<int:pk>/', views.CommissionDetailView.as_view(), name='commission_detail'),
+]
