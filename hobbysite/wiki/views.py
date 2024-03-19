ababb4267b6567286cc677f9b24e6091c74bed77
joshuasdasd <joshua.medina@student.ateneo.edu>
2024-03-19 23:47:55 +0800

    finished views

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Article
 
 # Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'wiki/article_list.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'wiki/article_detail.html'
