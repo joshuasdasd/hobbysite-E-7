14f533c4f81ca7cccf4147cdfc0b800ea0693317
joshuasdasd <joshua.medina@student.ateneo.edu>
2024-03-21 00:17:43 +0800

    fixed

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -42,10 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'merchstore',
-    'wiki'
+    'wiki',
     'blog',
-    'commissions'
-    'wikiblog'
+    'commissions',
 ]
 
 MIDDLEWARE = [
