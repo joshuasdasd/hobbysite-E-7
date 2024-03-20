29e5ea23102a27a01abd4f1bd86c1c7c9300e121
joshuasdasd <joshua.medina@student.ateneo.edu>
2024-03-20 23:54:43 +0800

    edited files

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -41,12 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
     'merchstore',
     'wiki'
-=======
     'blog',
->>>>>>> origin/blog
 ]
 
 MIDDLEWARE = [
