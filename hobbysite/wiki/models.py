e0d9cc6d33fe3a3837ba63771eb0a77575e936be
joshuasdasd <joshua.medina@student.ateneo.edu>
2024-05-07 15:30:38 +0800

    added models

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from user_management.models import Profile
 
 # Create your models here.
 
@@ -18,12 +19,14 @@ class ArticleCategory(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null = True)
     category = models.ForeignKey(
         ArticleCategory,
         on_delete=models.SET_NULL,
         null=True,
     )
     entry = models.TextField()
+    header_images = models.ImageField(upload_to='images/', null = True, blank = True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -35,3 +38,16 @@ class Article(models.Model):
     
     def get_absolute_url(self):
         return reverse('wiki:article_detail', args=[self.pk])
+    
+class Comment(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null = True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'Comment by {self.author}'
