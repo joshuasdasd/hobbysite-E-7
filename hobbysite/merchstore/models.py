39f696ad450ff215f125ed59b544fd6859b012f6
antukinskywalker <roxanne.silvallana@student.ateneo.edu>
2024-05-08 02:10:55 +0800

    Added fields to Product and made Transaction model

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -19,18 +19,57 @@ class Product(models.Model):
     product_type = models.ForeignKey(
         ProductType,
         on_delete=models.SET_NULL,
-        null=True,
+        null=True
+    )
+    owner = models.ForeignKey(
+        'user_management.Profile',
+        on_delete=models.CASCADE,
+        null=True
     )
     description = models.TextField()
     price = models.DecimalField(
         max_digits=10,
         decimal_places=2
     )
+    stock = models.PositiveIntegerField(null=True)
+    status_choices = [
+        ('Out of stock', 'Out of stock'),
+        ('Available', 'Available'),
+        ('On sale', 'On sale')
+    ]
+    status = models.CharField(
+        max_length=12, 
+        choices=status_choices, 
+        default='Available'
+    )
 
-    class Meta:
-        ordering = ['name']
-        verbose_name = 'Product'
-        verbose_name_plural = 'Products'
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    buyer = models.ForeignKey(
+        'user_management.Profile',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    amount = models.PositiveIntegerField()
+    status_choices = [
+        ('On cart', 'On cart'),
+        ('To pay', 'To pay'),
+        ('To ship', 'To ship'),
+        ('To receive', 'To receive'),
+        ('Delivered', 'Delivered')
+    ]
+    status = models.CharField(
+        max_length=10, 
+        choices=status_choices, 
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
