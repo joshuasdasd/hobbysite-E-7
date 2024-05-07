7035365b932a5b6b20ee3ca1be381dffc532f1e9
joshuasdasd <joshua.medina@student.ateneo.edu>
2024-05-07 16:12:10 +0800

    added urls for app level

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView
 
 urlpatterns =[
     path('articles/', ArticleListView.as_view(), name = 'article_list'),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name = 'article_detail'),
+    path('article/create/', ArticleCreateView.as_view(), name = 'article_create'),
+    path('article/update/<int:pk>/', ArticleUpdateView.as_view(), name = 'article_update'),
 ]
 
 app_name = 'wiki'
