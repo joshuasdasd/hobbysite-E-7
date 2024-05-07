75b98f5e9f14ab7ca7edc1f9305990b19c2da9b6
joshuasdasd <joshua.medina@student.ateneo.edu>
2024-05-08 00:44:33 +0800

    edited forms to only have specific fields

--- hobbysite/wiki/forms.py
+++ hobbysite/wiki/forms.py
@@ -4,15 +4,11 @@ from .models import Article, Comment
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = '__all__'
+        fields = ['title', 'category', 'entry', 'header_image']
     
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ['entry']
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+  
