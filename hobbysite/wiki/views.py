84aab80d7d0a63b0c4f57dfa7e489482e8a33584
joshuasdasd <joshua.medina@student.ateneo.edu>
2024-05-07 15:50:12 +0800

    added imports in views

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Article
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Article, ArticleCategory
+from .forms import ArticleForm, CommentForm
+from user_management.models import Profile
+from django.urls import reverse_lazy
 
 # Create your views here.
 class ArticleListView(ListView):
