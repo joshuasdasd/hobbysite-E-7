febdae30b58673b00f238e31edae8b5827cb1d3b
antukinskywalker <roxanne.silvallana@student.ateneo.edu>
2024-05-10 01:46:58 +0800

    Registered Transaction model, edited its html file

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, ProductType
+from .models import Product, ProductType, Transaction
 
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(ProductType)
+admin.site.register(Transaction)
