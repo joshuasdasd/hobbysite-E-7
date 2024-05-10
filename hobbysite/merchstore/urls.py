e71a6b52af7979c0af1f7f7bd659ceaf7d24b8b2
antukinskywalker <roxanne.silvallana@student.ateneo.edu>
2024-05-10 20:02:42 +0800

    Added cart view

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import index
-from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView
-from .views import TransactionListView
+from .views import *
 
 urlpatterns = [
     path('items/', ProductListView.as_view(), name='item_list'),
     path('item/<int:pk>/', ProductDetailView.as_view(), name='item_detail'),
     path('item/add/', ProductCreateView.as_view(), name='item_add'),
     path('item/<int:pk>/edit/', ProductUpdateView.as_view(), name='item_edit'),
+    path('cart/', CartListView.as_view(), name='cart_list'),
     path('transactions/', TransactionListView.as_view(), name='transaction_list')
     # path('', index, name='index'),
 ]
