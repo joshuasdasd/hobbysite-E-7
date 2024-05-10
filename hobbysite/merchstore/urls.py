7d4c1dd4d328892a33f08ff1b35497ccf48f3933
antukinskywalker <roxanne.silvallana@student.ateneo.edu>
2024-05-11 00:19:10 +0800

    Resolve conflicts

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import index
-from .views import ItemListView, ItemDetailView
+from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView
+from .views import TransactionListView
 
 urlpatterns = [
-    path('item/<int:pk>/', ItemDetailView.as_view(), name='item_detail'),
-    path('items/', ItemListView.as_view(), name='item_list'),
+    path('items/', ProductListView.as_view(), name='item_list'),
+    path('item/<int:pk>/', ProductDetailView.as_view(), name='item_detail'),
+    path('item/add/', ProductCreateView.as_view(), name='item_add'),
+    path('item/<int:pk>/edit/', ProductUpdateView.as_view(), name='item_edit'),
+    path('transactions/', TransactionListView.as_view(), name='transaction_list')
     # path('', index, name='index'),
 ]
 
--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -2,16 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Product
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from .models import Product, Transaction
 
 # Create your views here.
 def index(request):
     return HttpResponse('')
 
-class ItemListView(ListView):
+class ProductListView(ListView):
     model = Product
     template_name = 'merchstore/product_list.html'
 
-class ItemDetailView(DetailView):
+class ProductDetailView(DetailView):
     model = Product
     template_name = 'merchstore/product_detail.html'
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'merchstore/product_create.html'
+    fields = '__all__'
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'merchstore/product_update.html'
+
+class TransactionListView(ListView):
+    model = Transaction
+    template_name = 'merchstore/transaction_list.html'
